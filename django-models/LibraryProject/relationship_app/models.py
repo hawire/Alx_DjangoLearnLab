eb0474231a0371ae65a35373354f5d4098e37368
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-11-08 18:05:36 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -26,7 +27,7 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),

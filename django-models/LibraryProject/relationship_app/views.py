a5e64deb52b27b90efd0e805f66ac84a174af857
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-11-08 17:45:01 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # ✅ This line includes Library
+from .models import Book, Library 
 
 # Function-based view to list all books
 def list_books(request):

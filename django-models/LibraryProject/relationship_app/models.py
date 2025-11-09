be09aaebdf8183df5e7d318f360726b6142d9fdb
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-11-09 15:56:23 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,41 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import User
 
+# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
+# Book model with ForeignKey to Author
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return self.title
 
+
+# Library model with ManyToMany to Book
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book, related_name="libraries")
 
     def __str__(self):
         return self.name
 
+
+# Librarian model with OneToOne to Library
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
 
     def __str__(self):
         return self.name
-
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"

6452caab8c21cca0ad7d247f3aad0e8f1c77e202
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-11-09 16:11:16 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library   # <-- explicit import of Library
+from .models import Book, Library   # explicit import of Library
 
 # -------------------------
 # Function-based view

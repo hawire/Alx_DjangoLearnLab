817ed16569b4cf107c01e19516af97f66e65951d
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-11-23 15:08:10 +0300

    Update views.py

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from .models import Book
 
-# Example view to display all books
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+# Create your views here.

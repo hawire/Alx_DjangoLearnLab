f9faefd46acef41f1a3d49df7e46f92ee032d578
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-12-02 02:42:05 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,10 +9,12 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(default=timezone.now) 
     tags = TaggableManager()
 
     def __str__(self):
         return self.title
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,6 +23,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
 
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
